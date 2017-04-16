f3b11f9a233d22bffa2a793ee3e321f55a02a319
jndxhuxiehang <jndxhuxiehang@outlook.com>
2017-04-16 20:41:16 +0800

    Update updatelist.sh

--- updatelist.sh
+++ updatelist.sh
@@ -1,1 +1,9 @@
-# -dnslist-
+#!/bin/sh
+#
+sleep 2
+cd /etc/gw-redsocks/gw-shadowsocks
+echo "成功打开目录"
+echo "rm gw-shadowsocks.dnslist"
+echo "成功删除老版本dnslist文件"
+curl -o -k gw-ashadowsocks.dnslist https://g2w.online/dnsq/127.0.0.1:53535
+echo "成功下载新版本dnslist文件"
